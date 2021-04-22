7420a81dc3ad4ce8db5f628c1a83b0851e371d66
Nicholas Pilotto <nicholaspilotto@gmail.com>
2021-04-22 20:21:42 +0200

    Code formatting

--- lab01/automata.cpp
+++ lab01/automata.cpp
@@ -6,10 +6,11 @@ using namespace std;
 
 /**
  * Constructor for Abstract DFA.
- * 
+ *
  * @param noStates Number of _states in the DFA.
  */
-AbstractDFA::AbstractDFA(int noStates) : __states(noStates), __sink(false), __current(0) {
+AbstractDFA::AbstractDFA(int noStates)
+    : __states(noStates), __sink(false), __current(0) {
   // TODO: initialize data structures
   transitions = map<tpair, int>();
   final_states = map<int, bool>();
@@ -29,11 +30,12 @@ void AbstractDFA::reset() {
  * for the given letter, then the automaton proceeds to the successor state.
  * Otherwise it goes to the sink state. By construction it will stay in the
  * sink for every input letter.
- * 
+ *
  * @param letter The current input.
  */
 void AbstractDFA::doStep(char letter) {
-  // TODO: do step by going to the next state according to the current state and the read letter.
+  // TODO: do step by going to the next state according to the current state and
+  // the read letter.
   if (__current != -1) {
     map<tpair, int>::iterator a = transitions.find(tpair(letter, __current));
     if (a != transitions.end()) {
@@ -50,7 +52,7 @@ void AbstractDFA::doStep(char letter) {
 
 /**
  * Check if the automaton is currently accepting.
- * 
+ *
  * @return True, if the automaton is currently in the accepting state.
  */
 bool AbstractDFA::isAccepting() {
@@ -60,7 +62,7 @@ bool AbstractDFA::isAccepting() {
 
 /**
  * Run the DFA on the input.
- * 
+ *
  * @param inputWord stream that contains the input word
  * @return True, if if the word is accepted by this automaton
  */
@@ -77,7 +79,7 @@ bool AbstractDFA::run(const string& inputWord) {
  * "foo" the constructed automaton looks like: -> () -f-> () -o-> () -o-> []
  * from every state (including the final one) every other input letter leads
  * to a distinguished sink state in which the automaton then remains
- * 
+ *
  * @param word A String that the automaton should recognize
  */
 WordDFA::WordDFA(const string& word) : AbstractDFA(word.size() + 2) {
@@ -121,12 +123,13 @@ CommentDFA::CommentDFA() : AbstractDFA(0) {
 /**
  * Performs one step of the DFA for a given letter. This method works
  * differently than in the superclass AbstractDFA.
- * 
+ *
  * @param letter
  *            The current input.
  */
 void CommentDFA::doStep(char letter) {
   // TODO: implement accordingly
+
   if (__current != -1) {
     if (__sink) {
       __current = -1;
@@ -139,7 +142,8 @@ void CommentDFA::doStep(char letter) {
           __current = 0;
         } else if (__current == 6) {
           __current = 5;
-        } else if (__current == 0 || __current == 3 || __current == 7 || __current == 9) {
+        } else if (__current == 0 || __current == 3 || __current == 7 ||
+                   __current == 9) {
           __sink = true;
         }
       }
